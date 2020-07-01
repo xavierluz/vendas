bb81aa0b3d37093e40c42a226a185b7d9686b183
Celso Xavier Luz <xavierluz@gmail.com>
2020-07-01 15:39:43 -0300

    20200701-Projeto criado

---
+++ src/main/java/com/xavierluzvendas/vendasmvc/VendasmvcApplication.java
@@ -0,0 +1,13 @@
+package com.xavierluzvendas.vendasmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xavierluzvendas/vendasmvc/VendasmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavierluzvendas.vendasmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
