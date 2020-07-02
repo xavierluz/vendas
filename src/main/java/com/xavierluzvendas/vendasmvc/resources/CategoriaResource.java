a185cbf384f3376ac3944675b3820d3cf5dbb932
Celso Xavier Luz <xavierluz@gmail.com>
2020-07-02 09:52:28 -0300

    20200702 - Classe de dominio de categoria

--- src/main/java/com/xavierluzvendas/vendasmvc/resources/CategoriaResource.java
+++ src/main/java/com/xavierluzvendas/vendasmvc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.xavierluzvendas.vendasmvc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.xavierluzvendas.vendasmvc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET)
-	public String Listar() {
-		return "REST está funcionando";
+	public List<Categoria> Listar() {
+		
+		Categoria categoria1 = new Categoria(1,"Informática");
+		Categoria categoria2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(categoria1);
+		lista.add(categoria2);
+		
+		return lista;
 	}
 
 }
