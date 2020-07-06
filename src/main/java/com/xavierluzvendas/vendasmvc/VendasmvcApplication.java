14266af1b05815a3a89e3f83d26dda01c740e37b
Celso Xavier Luz <xavierluz@gmail.com>
2020-07-06 19:13:20 -0300

    20200706 - Categoria Create ao inicializar

--- src/main/java/com/xavierluzvendas/vendasmvc/VendasmvcApplication.java
+++ src/main/java/com/xavierluzvendas/vendasmvc/VendasmvcApplication.java
@@ -1,13 +1,30 @@
 package com.xavierluzvendas.vendasmvc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.xavierluzvendas.vendasmvc.domain.Categoria;
+import com.xavierluzvendas.vendasmvc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class VendasmvcApplication {
+public class VendasmvcApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasmvcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+
+		this.categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 	}
 
 }
